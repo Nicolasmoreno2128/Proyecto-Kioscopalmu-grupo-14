dec2e0785b97f2a4aa3d8a80515cbb59348f4c4d
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-06-29 21:17:06 -0300

    Validacion fecha.cpp
    Se crea validaciones para que no se coloquen numeros negativos ni letras en los campos de fecha

--- Kioscopalmu/Fecha.h
+++ Kioscopalmu/Fecha.h
@@ -16,9 +16,38 @@ public:
     Fecha() : dia(1), mes(1), anio(2025) {}
 
     void Cargar(){
-        cout<<"DIA ";cin>>dia;
-        cout<<"MES ";cin>>mes;
-        cout<<"ANIO ";cin>>anio;
+// Día
+    do {
+        cout << "DIA (1-31): ";
+        cin >> dia;
+        if (cin.fail() || dia < 1 || dia > 31) {
+            cout << "Día inválido. Ingrese un número entre 1 y 31.\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        } else break;
+    } while (true);
+
+    // Mes
+    do {
+        cout << "MES (1-12): ";
+        cin >> mes;
+        if (cin.fail() || mes < 1 || mes > 12) {
+            cout << "Mes inválido. Ingrese un número entre 1 y 12.\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        } else break;
+    } while (true);
+
+    // Año
+    do {
+        cout << "AÑO (2000-2100): ";
+        cin >> anio;
+        if (cin.fail() || anio < 2000 || anio > 2100) {
+            cout << "Año inválido. Ingrese un número entre 2000 y 2100.\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        } else break;
+    } while (true);
     }
     void Mostrar(){
         cout<<dia<<"/";
